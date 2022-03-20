d041ee5270b60d72efcff180d09a8308dfbde1db
mzawadi <mwaranguallan345@gmail.com>
2022-03-20 11:43:22 +0300

    add Phantom Wallet Integration

--- index.js
+++ index.js
@@ -13,10 +13,23 @@ async function getAccount() {
   document.querySelector('#showAccount').innerHTML = account;
 }
 
-if (typeof window.solana.isPhantom !== 'undefined') {
+if (window.solana !== 'undefined') {
   console.log('Phantom is installed!');
 }
 
 function SolButtonClick(params) {
-  
+  getSolAccount();
+}
+
+async function getSolAccount() {
+  try {
+    const resp = await window.solana.connect();
+    const PhantomAccount = resp.publicKey.toString()
+    console.log(PhantomAccount);
+    document.querySelector('#showSolAccount').innerHTML = PhantomAccount;
+    // 26qv4GCcx98RihuK3c4T6ozB3J7L6VwCuFVc7Ta2A3Uo 
+  } catch (err) {
+    console.log(err);
+    // { code: 4001, message: 'User rejected the request.' }
+  }
 }
